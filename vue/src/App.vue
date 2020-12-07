3ed500410b4c291c3d77f2ec006a19719bfe0c13
eseyas.asmelash <ezaweld@gmail.com>
2020-12-07 15:06:51 +0100

    first commit

---
+++ vue/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+    <hello-world />
+</template>
+
+<script>
+//import { component } from "vue/types/umd";
+import HelloWorld from "./components/HelloWorld"
+
+/*import Vue from 'vue'
+import msal from 'vue-msal'
+
+
+ 
+Vue.use(msal, {
+    auth: {
+      clientId: '',
+      
+    }
+});
+
+new Vue({
+
+    created() {
+        if (!this.$msal.isAuthenticated()) {
+            this.$msal.sigIn();
+        }
+    }
+});*/
+
+export default {
+    name: "app",
+  components: {
+    HelloWorld
+  }
+}
+
+</script>
---
+++ vue/src/components/HelloWorld.vue
@@ -0,0 +1,44 @@
+
+<template>
+  <div class="pt-5">
+    <div class="border col-md-2 col-sm-12 m-auto p-5 text-info bg-light pt-5">
+      <h2> Sign In </h2>
+      <hr />
+      <div >
+          <label for="emailId">Email Address</label>
+          <input v-model="emailId" type="text" id="emailId" name="emailId" required>
+      </div>
+
+      <div id="inp">
+          <label for="pass">Password</label>
+          <input v-model="password" type="password" id="pass" name="pass" minlength="8" required>
+      </div>
+      <button v-on:click="SignIn" class="btn btn-info mt-4 px-2" type="submit">Sign In</button>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      emailId : "",
+      password : ""
+    }
+
+  },
+  methods: {
+    signIn() {
+
+    }
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped> 
+
+</style>
